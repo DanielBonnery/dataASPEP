69eab3d634b4c46dc187b9e5936e332aca093d57
DanielBonnery <daniel.bonnery@gmail.com>
2016-05-18 14:49:38 -0400

    a

--- demo/get_data_from_web.R
+++ demo/get_data_from_web.R
@@ -1,63 +1,35 @@
-get_data_from_web<-function(){
-idformat<-read.csv(system.file("extdata","id_format.csv",package="dataASPEP"))
-dataformat<-read.csv(system.file("extdata","data_format.csv",package="dataASPEP"))
-codes_in_web_files=read.csv(system.file("extdata","codes_in_web_files.csv",package="dataASPEP"),colClasses = "character")
-
-years<-c(2007,2009:2012)
-
-
-itemcodeoriginalcoding<-data.frame(variable="itemcode",
-                                   levels=
-                                     c("000","001","002","005","006","012","112","014","016","018","021","022","023","024","124","025","029","032","040","044","050","052","059","061","062","162","079","080","081","087","089","090","091","092","093","094"),
-                                   labels=c("Totals for Government ",	"Airports ",	"Space Research & Technology (Federal) ",	"Correction ",	"National Defense and International Relations (Federal) ",	"Elementary and Secondary - Instruction ",	"Elementary and Secondary - Other Total ",	"Postal Service (Federal) ",	"Higher Education - Other ",	"Higher Education - Instructional ",	"Other Education (State) ",	"Social Insurance Administration (State) ",	"Financial Administration ",	"Firefighters ",	"Fire - Other ",	"Judical & Legal ",	"Other Government Administration ",	"Health ",	"Hospitals ",	"Streets & Highways ",	"Housing & Community Development (Local) ",	"Local Libraries ",	"Natural Resources ",	"Parks & Recreation ",	"Police Protection - Officers ",	"Police-Other ",	"Welfare ",	"Sewerage ",	"Solid Waste Management ",	"Water Transport & Terminals ",	"Other & Unallocable ",	"Liquor Stores (State) ",	"Water Supply ",	"Electric Power ",	"Gas Supply ",	"Transit "))
-stateoriginalcoding<-data.frame(variable="state",levels=sprintf("%02d", 1:51),
-                                labels=c(state.name[1:8],"District of Columbia",state.name[9:50]))
-typeoriginalcoding<-data.frame(variable="type_of_gov",levels=as.character(0:6),
-                               labels=c("State government","County government","Municipal government","Township government","Special district government","School district government","Federal Government"))
-codes_in_web_files=rbind(itemcodeoriginalcoding,stateoriginalcoding,typeoriginalcoding)
-write.csv(codes_in_web_files,file="inst/extdata/codes_in_web_files.csv")
-read.csv("inst/extdata/codes_in_web_files.csv",colClasses = "character")
-
-
-get_data_from_webs<-function(webfile,format.table){
-  tmpf  <-tempfile()
-  download.file(file.path("http://www2.census.gov/govs/apes/",webfile),tmpf)
-  x=unzip(tmpf,exdir = tempdir())
-  y<-read.fwf(x,width=format.table$length,
-              header=FALSE,
-              colClasses=c("character","numeric","numeric")[format.table$class],fill=TRUE)
-  y<-y[format.table$variable!=""]
-  names(y)<-format.table$variable[format.table$variable!=""]
-y}
-reformat<-function(y){
-  y$id=paste0(y$state,y$type_of_gov,y$county,y$unit_identification_number,y$supp_code,y$sub_code)
-  y$itemcode<-factor(y$itemcode,levels=itemcodeoriginalcoding$levels,labels=itemcodeoriginalcoding$labels)
-y[c("id","itemcode","ftemp","fteemp","ftpay","ptemp","ptpay","pthours","fte")]}
-reformat_gov<-function(y){
-  y$id=paste0(y$state,y$type_of_gov,y$county,y$unit_identification_number,y$supp_code,y$sub_code)
-  y$state      <-factor(y$state,levels=stateoriginalcoding$levels,labels=stateoriginalcoding$labels)
-  y$type_of_gov<-factor(y$type_of_gov,levels=typeoriginalcoding$levels,labels=typeoriginalcoding$labels)
-  y}
-
-
-aspep2007    <-reformat(get_data_from_webs("07cempst.zip",dataformat))
-aspep2007_gov<-reformat_gov(get_data_from_webs("07cempid.zip",idformat))
-aspep2009    <-reformat(get_data_from_webs("09empst.zip",dataformat))
-aspep2009_gov<-reformat_gov(get_data_from_webs("09empid.zip",idformat))
-aspep2010    <-reformat(get_data_from_webs("10empst.zip",dataformat))
-aspep2010_gov<-reformat_gov(get_data_from_webs("10empid.zip",idformat))
-aspep2011    <-reformat(get_data_from_webs("11empst.zip",dataformat))
-aspep2011_gov<-reformat_gov(get_data_from_webs("11empid.zip",idformat))
-aspep2012    <-reformat(get_data_from_webs("12cempst.zip",dataformat))
-aspep2012_gov<-reformat_gov(get_data_from_webs("12cempid.zip",idformat))
-
-
-listofids<-unique(unlist(lapply(grep("_gov",ls(),value = TRUE)[-6],function(x){get(x)$id})))
-
-z=find.package("dataASPEP")
-for (x in (grep("aspep",ls(),value = TRUE))){
-  eval(parse(text=paste0(x,"$id=factor(",x,"$id,levels=listofids);save(",x,",file=file.path(z,'data/",x,".rda')")))}
-cat(paste(paste0("aspep",outer(years,c("","_gov"),paste0)),collapse="\n"),
-    file=file.path(z,'data/datalist'))
-
-}
+get_data_from_web<-function(savetopackage=TRUE){
+  codes_in_web_files=read.csv(system.file("extdata","codes_in_web_files.csv",package="dataASPEP"),colClasses = "character")
+  years<-c(2007,2009:2012)
+  reformat<-function(y){
+    y$id=paste0(y$state,y$type_of_gov,y$county,y$unit_identification_number,y$supp_code,y$sub_code)
+    for (x in intersect(unique(codes_in_web_files$variable),names(y))){
+      ref<-codes_in_web_files[codes_in_web_files$variable==x,]
+      y[x]<-factor(y[x],levels=ref$levels,labels=ref$labels)}
+    y[if(is.element("itemcode",names(y))){c("id","itemcode","ftemp","fteemp","ftpay","ptemp","ptpay","pthours","fte")}else{names(y)}]
+  }
+  get_data_from_webs<-function(webfile,format.table){
+    tmpf  <-tempfile()
+    download.file(file.path("http://www2.census.gov/govs/apes/",webfile),tmpf)
+    x=unzip(tmpf,exdir = tempdir())
+    y<-read.fwf(x,width=format.table$length,
+                header=FALSE,
+                colClasses=c("character","numeric","numeric")[format.table$class],fill=TRUE)
+    y<-y[format.table$variable!=""]
+    names(y)<-format.table$variable[format.table$variable!=""]
+    reformat(y)}
+  L1<-lapply(c("07cempst.zip","09empst.zip","10empst.zip","11empst.zip","12cempst.zip"),get_data_from_webs,
+         format.table=read.csv(system.file("extdata","data_format.csv",package="dataASPEP")))
+  L2<-lapply(c("07cempid.zip","09empid.zip","10empid.zip","11empid.zip","12cempid.zip"),get_data_from_webs,
+         format.table=read.csv(system.file("extdata","id_format.csv",package="dataASPEP")))
+  names(L1)<-paste0("aspep",years)
+  names(L2)<-paste0("aspep",years,"_gov")
+  listofids<-unique(unlist(lapply(L1,function(x){x$id})))
+  attach(L1);attach(L2)
+  if(savetopackage){
+    z=find.package("dataASPEP")
+    if(!file.exists(file.path(z,'data'))){dir.create(file.path(z,'data'))}
+    cat(paste(paste0("aspep",outer(years,c("","_gov"),paste0)),collapse="\n"),file=file.path(z,'data/datalist'))}
+  for (x in c(names(L1),names(L2))){
+    eval(parse(text=paste0(x,"$id=factor(",x,"$id,levels=listofids);if(savetopackage){save(",x,",file=file.path(z,'data/",x,".rda'))}")))}
+  if(!savetopackage){return(c(L1,L2))}}
