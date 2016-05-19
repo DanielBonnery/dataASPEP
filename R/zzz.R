3cc20c7ff24be27b5613b4af84a1783b8c74194b
DanielBonnery <daniel.bonnery@gmail.com>
2016-05-18 20:48:58 -0400

    a

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 .onLoad<- function(libname, pkgname) {
   if(!all(is.element(paste0("aspep",outer(c(2007,2009:2012),c("","_gov"),paste0)),
-                     data(package="dataASPEP")$results[, "Item"]))){
-    packageStartupMessage("This is the first time you load dataASPEP.
-                          Data is going to be downloaded from the Census Website.
-                          You need a connection to the web.")
-    
+                     list.files(file.path(find.package("dataASPEP"),"data"))))){
+    packageStartupMessage("This is the first time the package dataASPEP is loaded. Data is going to be downloaded from the Census Website.
+                          A connection to the web is needed.")
     get_data_from_web()
   }}
